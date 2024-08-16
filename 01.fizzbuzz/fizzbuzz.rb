a9b45467d594134c09d472e64851f15348ac0a58
SuzukiShuntarou <da8801593@gmail.com>
2024-08-16 22:55:02 +0900

    Change to each method

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,13 +1,13 @@
-i = 1
-while i <= 20
-    if i % 15 == 0
-        puts 'FizzBuzz'
-    elsif i % 3 == 0
-        puts 'Fizz'
-    elsif i % 5 == 0
-        puts 'Buzz'
-    else
-        puts i
-    end
-    i += 1
+#!/usr/bin/env ruby
+
+(1..20).each do |i|
+  if i % 15 == 0
+    puts 'FizzBuzz'
+  elsif i % 3 == 0
+    puts 'Fizz'
+  elsif i % 5 == 0
+    puts 'Buzz'
+  else
+    puts i
+  end
 end
