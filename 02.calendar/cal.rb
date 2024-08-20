f408e11b6fbe1f00958d96a74f18a8277b3cafdd
SuzukiShuntarou <da8801593@gmail.com>
2024-08-20 22:56:29 +0900

    Add Condtion the argument of Date & Change Calendar-method

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -11,37 +11,27 @@ opt.on('-y'){ |year|  params[:y] = year }
 #引数の取り出し
 opt.parse!(ARGV)
 
-#引数が-y,-mどちらも渡されないと今日の日付をcalenderメソッドへ渡す
-if params[:m] && params[:y]
+if params[:m] && params[:y] #引数が-y,-mどちらも渡された時
   year  = opt.parse!(ARGV).map!(&:to_i).max
   month = opt.parse!(ARGV).map!(&:to_i).min
-else
+elsif params[:m]            #月だけの場合は今年の月を返す
+  year  = Time.now.year
+  month = opt.parse!(ARGV).map!(&:to_i)[0]
+else                        #引数がない場合は今日の日付を返す
   year  = Time.now.year
   month = Time.now.month
 end
 
 #calenderメソッド
-def calendar(y, m)
-  puts " #{m}月 #{y}年\n 日 月 火 水 木 金 土"
-  #月の日数をカウントする変数mday
-  mday = 1 
-  #月の開始曜日前に半角スペースを追加
-  unless Date.new(y, m, mday).sunday?
-    print "   " * Date.new(y, m).cwday
-  end
-  while mday <= Date.new(y, m, -1).day
-    if !Date.new(y, m, mday).saturday?
-      if mday < 10
-        print "  #{mday}"
-      elsif
-        print " #{mday}"
-      end
-    elsif mday < 10
-      puts "  #{mday}"
-    else
-      puts " #{mday}"
-    end
-    mday += 1
+def calendar(year, month)
+  count_day = Date.new(year, month, 1)        #カウント用、初期値は指定月の1日
+  last_day = Date.new(year, month, -1).day    #月の最終日を定義
+  puts " #{month}月 #{year}年\n 日 月 火 水 木 金 土"
+  print "   " * Date.new(year, month).wday    #月の開始曜日前に半角スペースを追加
+  (1..last_day).each do |day|                 #1日から最終日までの数字を表示
+    print "#{day}".rjust(3)
+    puts "" if count_day.saturday?            #土曜日で改行
+    count_day += 1
   end
 end
 
