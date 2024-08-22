c0b0f90f524beffabb437bb4ce90ae758377e085
SuzukiShuntarou <da8801593@gmail.com>
2024-08-22 21:07:47 +0900

    Change Argument options and Add first_day

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -11,27 +11,19 @@ opt.on('-y'){ |year|  params[:y] = year }
 #引数の取り出し
 opt.parse!(ARGV)
 
-if params[:m] && params[:y] #引数が-y,-mどちらも渡された時
-  year  = opt.parse!(ARGV).map!(&:to_i).max
-  month = opt.parse!(ARGV).map!(&:to_i).min
-elsif params[:m]            #月だけの場合は今年の月を返す
-  year  = Time.now.year
-  month = opt.parse!(ARGV).map!(&:to_i)[0]
-else                        #引数がない場合は今日の日付を返す
-  year  = Time.now.year
-  month = Time.now.month
-end
+#引数が与えられた時だけ指定の年、月とする
+year = params[:y] ? opt.parse!(ARGV).map!(&:to_i).max : Time.now.year
+month = params[:m] ? opt.parse!(ARGV).map!(&:to_i).min : Time.now.month
 
 #calenderメソッド
 def calendar(year, month)
-  count_day = Date.new(year, month, 1)        #カウント用、初期値は指定月の1日
-  last_day = Date.new(year, month, -1).day    #月の最終日を定義
+  first_day = Date.new(year, month, 1)      #月の初日
+  last_day = Date.new(year, month, -1)      #月の最終日
   puts " #{month}月 #{year}年\n 日 月 火 水 木 金 土"
-  print "   " * Date.new(year, month).wday    #月の開始曜日前に半角スペースを追加
-  (1..last_day).each do |day|                 #1日から最終日までの数字を表示
-    print "#{day}".rjust(3)
-    puts "" if count_day.saturday?            #土曜日で改行
-    count_day += 1
+  print "   " * Date.new(year, month).wday  #月の開始曜日前に半角スペースを追加
+  (first_day..last_day).each do |date|      #1日から最終日までの数字を表示
+    print "#{date.day}".rjust(3)
+    puts  if date.saturday?                 #土曜日で改行
   end
 end
 
