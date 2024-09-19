7b9a4bb6c8bbc79e012ec30fbc54734949dfed00
SuzukiShuntarou <da8801593@gmail.com>
2024-09-19 20:39:27 +0900

    Change Comment & variable names & each_slice method

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -9,24 +9,22 @@ def main
 end
 
 def show_file_list(filenames)
-  maximum_length = filenames.max_by(&:length).length
+  max_length = filenames.max_by(&:length).length
 
-  nested_file_names = transpose_nested_filenames(filenames)
+  nested_file_names = convert_nested_filenames(filenames)
   nested_file_names.each do |file_names|
     file_names.each do |file_name|
-      print file_name.ljust(maximum_length + 2) unless file_name.nil?
+      print file_name.ljust(max_length + 2) unless file_name.nil?
     end
     puts
   end
 end
 
-def transpose_nested_filenames(filenames)
-  # 転置後の行列の列の数が指定の値になるように行の数を求める。
+def convert_nested_filenames(filenames)
   row_count = filenames.length.ceildiv(COLUMN_COUNT)
 
-  nested_file_names = []
-  filenames.each_slice(row_count) { |filename| nested_file_names << filename }
-  # 転置行列を作成するため、二次元配列の最後の要素に不足分があればnilで埋め尽くす。
+  nested_file_names = filenames.each_slice(row_count).to_a
+  # 各要素のサイズが不揃いだとtransposeメソッドでエラーが発生するのでサイズを揃える
   nil_count = row_count - nested_file_names.last.length
   nested_file_names.last.concat(Array.new(nil_count))
 
