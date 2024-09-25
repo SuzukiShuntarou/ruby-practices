b2e6f492895520e582b911cc2c00c3da886206c5
SuzukiShuntarou <da8801593@gmail.com>
2024-09-25 21:27:20 +0900

    Change argument assignment

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -12,7 +12,8 @@ def main
 
   option.parse!(ARGV)
   filenames = options[:all] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
-  options[:reverse] ? show_file_list(filenames.reverse) : show_file_list(filenames)
+  filenames = options[:reverse] ? filenames.reverse : filenames
+  show_file_list(filenames)
 end
 
 def show_file_list(filenames)
