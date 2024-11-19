de47550b8b2bb93468da9ca339de41f8bf2ac902
SuzukiShuntarou <da8801593@gmail.com>
2024-11-20 00:21:48 +0900

    Delete BonusScore Class & Change responsibilities of Frame Class

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -1,17 +1,30 @@
 # frozen_string_literal: true
 
 class Frame
-  def initialize(args)
-    @first_shot = args[:first_shot]
-    @second_shot = args[:second_shot]
-    @third_shot = args[:third_shot] || default_score
+  def initialize(point, frame_count, now_frame, next_frame, frame_after_next)
+    @point = point
+    @frame_count = frame_count
+    @now_frame = now_frame
+    @next_frame = next_frame
+    @frame_after_next = frame_after_next
   end
+
+  BEFORE_FINAL_FRAME = 9
+  FINAL_FRAME = 10
 
   def score
-    [@first_shot, @second_shot, @third_shot].sum
-  end
-
-  def default_score
-    0
+    @point +=
+      if @point == 10 && @frame_count != FINAL_FRAME
+        @next_frame[:first_shot] +
+          if @now_frame[:first_shot] != 10
+            0
+          elsif @next_frame[:first_shot] != 10 || @frame_count == BEFORE_FINAL_FRAME
+            @next_frame[:second_shot]
+          else
+            @frame_after_next[:first_shot]
+          end
+      else
+        0
+      end
   end
 end
