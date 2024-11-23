ee13ffa4da66fb3e1d2f9a057b651d921437e226
SuzukiShuntarou <da8801593@gmail.com>
2024-11-23 19:45:06 +0900

    Change variable name in the test file

--- 06.bowling_object/test/test_bowling.rb
+++ 06.bowling_object/test/test_bowling.rb
@@ -5,42 +5,42 @@ require 'minitest/autorun'
 
 class BowlingTest < Minitest::Test
   def test_pattern_one
-    game_pattern_one = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
-    assert_equal 139, game_pattern_one.calculate_score
+    game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
+    assert_equal 139, game.calculate_score
   end
 
   def test_pattern_two
-    game_pattern_two = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
-    assert_equal 164, game_pattern_two.calculate_score
+    game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
+    assert_equal 164, game.calculate_score
   end
 
   def test_pattern_three
-    game_pattern_three = Game.new('0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4')
-    assert_equal 107, game_pattern_three.calculate_score
+    game = Game.new('0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4')
+    assert_equal 107, game.calculate_score
   end
 
   def test_pattern_four
-    game_pattern_four = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0')
-    assert_equal 134, game_pattern_four.calculate_score
+    game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0')
+    assert_equal 134, game.calculate_score
   end
 
   def test_pattern_five
-    game_pattern_five = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8')
-    assert_equal 144, game_pattern_five.calculate_score
+    game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8')
+    assert_equal 144, game.calculate_score
   end
 
   def test_perfect_game
-    perfect_game = Game.new('X,X,X,X,X,X,X,X,X,X,X,X')
-    assert_equal 300, perfect_game.calculate_score
+    game = Game.new('X,X,X,X,X,X,X,X,X,X,X,X')
+    assert_equal 300, game.calculate_score
   end
 
   def test_almost_perfect_game
-    almost_perfect_game = Game.new('X,X,X,X,X,X,X,X,X,X,X,2')
-    assert_equal 292, almost_perfect_game.calculate_score
+    game = Game.new('X,X,X,X,X,X,X,X,X,X,X,2')
+    assert_equal 292, game.calculate_score
   end
 
   def test_spare_and_zero_game
-    spare_and_zero_game = Game.new('X,0,0,X,0,0,X,0,0,X,0,0,X,0,0')
-    assert_equal 50, spare_and_zero_game.calculate_score
+    game = Game.new('X,0,0,X,0,0,X,0,0,X,0,0,X,0,0')
+    assert_equal 50, game.calculate_score
   end
 end
