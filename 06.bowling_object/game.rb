2a450246098894d1b5da33da490e9ca001eabcb2
SuzukiShuntarou <da8801593@gmail.com>
2024-11-27 22:43:16 +0900

    Change to create Frame-object only 10 times

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -10,12 +10,12 @@ class Game
   end
 
   def calculate_score
-    nested_shots = build_shots
+    nested_frames = build_frames
 
-    nested_shots.each_with_index.sum do |shots, count|
-      current_frame = Frame.new(shots)
-      next_frame = Frame.new(nested_shots[count + 1]) if count < 9
-      after_next_frame = Frame.new(nested_shots[count + 2]) if count < 8
+    nested_frames.each_with_index.sum do |frames, count|
+      current_frame = frames
+      next_frame = nested_frames[count + 1] if count < 9
+      after_next_frame = nested_frames[count + 2] if count < 8
 
       current_frame.calculate_score(next_frame, after_next_frame)
     end
@@ -23,21 +23,21 @@ class Game
 
   private
 
-  def build_shots
+  def build_frames
     shots = @marks.split(',').map { |mark| Shot.new(mark) }
-    nested_shots = []
+    nested_frames = []
 
-    count_by_frame = 0
-    9.times do
-      if shots[count_by_frame].strike?
-        nested_shots << [shots[count_by_frame]]
+    count_by_shot = 0
+    9.times do |count_by_frame|
+      if shots[count_by_shot].strike?
+        nested_frames[count_by_frame] = Frame.new([shots[count_by_shot]])
       else
-        nested_shots << [shots[count_by_frame], shots[count_by_frame + 1]]
-        count_by_frame += 1
+        nested_frames[count_by_frame] = Frame.new([shots[count_by_shot], shots[count_by_shot + 1]])
+        count_by_shot += 1
       end
-      count_by_frame += 1
+      count_by_shot += 1
     end
-    nested_shots << shots[count_by_frame..]
+    nested_frames << Frame.new(shots[count_by_shot..])
   end
 end
 
