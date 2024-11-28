de8ac1cc14d3015a802b10a263dc365fc2004a91
SuzukiShuntarou <da8801593@gmail.com>
2024-11-29 00:13:35 +0900

    Add calculate_bonus_score method

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -9,15 +9,20 @@ class Frame
   end
 
   def calculate_score(next_frame, after_next_frame)
+    sum_shots + calculate_bonus_score(next_frame, after_next_frame)
+  end
+
+  private
+
+  def calculate_bonus_score(next_frame, after_next_frame)
     following_shots = [next_frame, after_next_frame].compact.flat_map(&:shots)
-    sum_shots +
-      if open_frame? || !next_frame
-        0
-      elsif spare?
-        following_shots[0].score
-      elsif strike?
-        following_shots.first(2).sum(&:score)
-      end
+    if open_frame? || !next_frame
+      0
+    elsif spare?
+      following_shots[0].score
+    elsif strike?
+      following_shots.first(2).sum(&:score)
+    end
   end
 
   def strike?
