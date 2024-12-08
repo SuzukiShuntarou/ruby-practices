2154abcf5417bdb70cded456713a4926a5c47a21
SuzukiShuntarou <da8801593@gmail.com>
2024-12-08 17:50:07 +0900

    Add LsOption module

--- 07.ls_object/ls.rb
+++ 07.ls_object/ls.rb
@@ -1,52 +1,31 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require_relative 'lsoption/option'
 require_relative 'directory'
 require_relative 'directoryformatter'
 require_relative 'file'
-require 'optparse'
 
 class Ls
   def initialize(argv = nil)
-    parse_options(argv)
+    LsOption.parse(argv)
   end
 
   def show_file_list
-    puts build_directory.format(long?)
+    puts build_directory.format(LsOption.long?)
   end
 
   private
 
   def build_directory
     filenames = []
-    all? ? Dir.foreach('.') { |file| filenames << file } : filenames = Dir.glob('*')
+    LsOption.all? ? Dir.foreach('.') { |file| filenames << file } : filenames = Dir.glob('*')
     sorted_filenames = filenames.sort_by { |filename| filename.sub(/^\./, '').downcase }
 
     files = sorted_filenames.map do |filename|
       CustomFile::File.new(filename)
     end
-    DirectoryFormatter.new(reverse? ? files.reverse : files)
-  end
-
-  def all?
-    @options[:all]
-  end
-
-  def reverse?
-    @options[:reverse]
-  end
-
-  def long?
-    @options[:long]
-  end
-
-  def parse_options(argv)
-    option = OptionParser.new
-    @options = {}
-    option.on('-a') { @options[:all] = true }
-    option.on('-r') { @options[:reverse] = true }
-    option.on('-l') { @options[:long] = true }
-    option.parse!(argv)
+    DirectoryFormatter.new(LsOption.reverse? ? files.reverse : files)
   end
 end
 
